be8a9e292244c01f5d95c31b3ffcfb1fc3c1bc95
rahul-reddy45 <22501a4445@pvpsit.ac.in>
2025-03-09 16:50:46 +0530

    changed

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,7 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
 	const io = new Server(server, {
 		cors: {
-			origin: "https://localhost:3000",
+			origin: "http://localhost:3000",
 			methods: ["GET", "POST"],
 			credentials: true,
 		},
