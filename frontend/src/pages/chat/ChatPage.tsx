6049c7800caf1f3c5de31188e1aebdf8b2a16fb7
rahul-reddy45 <22501a4445@pvpsit.ac.in>
2025-03-20 02:40:12 +0530

    p

--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -92,7 +92,7 @@ export default ChatPage;
 
 const NoConversationPlaceholder = () => (
 	<div className='flex flex-col items-center justify-center h-full space-y-6'>
-		<img src='/path/to/profilee.png' alt='Spotify' className='size-16 animate-bounce' />
+		<img src='https://i.pinimg.com/736x/98/09/90/980990e79f336bbf2230e194487f5286.jpg' alt='Spotify' className='size-16 animate-bounce' />
 		<div className='text-center'>
 			<h3 className='text-zinc-300 text-lg font-medium mb-1'>No conversation selected</h3>
 			<p className='text-zinc-500 text-sm'>Choose a friend to start chatting</p>
