cd7c66dd9964d4b873475ee8f04102a561f3e069
rahul-reddy45 <22501a4445@pvpsit.ac.in>
2025-03-20 01:56:23 +0530

    change of profile path

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -17,7 +17,7 @@ const Topbar = () => {
     '
 		>
 			<div className='flex gap-2 items-center'>
-				<img src='/profilee.png' className='size-8' alt='Spotify logo' />
+				<img src='/path/to/profilee.png' className='size-8' alt='Profile icon' />
 				ꜱᴏɴɪQ
 			</div>
 			<div className='flex items-center gap-4'>
